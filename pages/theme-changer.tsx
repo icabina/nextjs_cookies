ffc3381875022305a0a50fb1c7112b2088d0553f
Jorge Roldán <icabina@live.com>
2022-06-09 17:12:32 -0500

    m

---
+++ pages/theme-changer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout } from '../components/layouts'
+
+const ThemeChanger = () => {
+  return (
+    <Layout>
+        <h1>ThemeChanger</h1>
+    </Layout>
+  )
+}
+
+export default ThemeChanger
